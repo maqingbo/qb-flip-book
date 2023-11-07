0164353832ff3fd4c43c0a454bbdd28ebbfe66ba
maqingbo <wmaqingbo@163.com>
2023-11-07 15:30:51 +0800

    first commit

---
+++ examples/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <PreviewCom01 msg="qb-flip-book" />
+</template>
+
+<script setup>
+import PreviewCom01 from './components/PreviewCom01.vue'
+</script>
+
+<style scoped lang="scss"></style>
---
+++ examples/src/components/PreviewCom01.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="bookArea">{{ msg }}</div>
+</template>
+
+<script setup>
+// import { PageFlip } from '../../../src/main.ts'
+import { PageFlip } from '../../../dist/qb-flip-book.js'
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const bookArea = ref(null)
+const obj = new PageFlip(bookArea.value, {
+  width: 500,
+  height: 700,
+  showCover: false,
+})
+console.log('%c PageFlip: ', 'background-color: pink', obj)
+</script>
+
+<style scoped lang="scss"></style>
